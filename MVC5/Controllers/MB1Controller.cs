8692de7dc3da3d22ce981402e37b5997f3445265
jellus156 <jellus156@gmail.com>
2016-05-19 11:59:35 +0800

    2016/05/19

--- MVC5/Controllers/MB1Controller.cs
+++ MVC5/Controllers/MB1Controller.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -39,6 +40,105 @@ namespace MVC5.Controllers
             ViewData["Message3"] = Session["Message3"];
 
             return View();
+        }
+
+        public ActionResult Smple1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Smple1(string UserName, string Password, string ConfirmPassword)
+        {
+            return Content(UserName +"/" + Password +"/"+ ConfirmPassword);
+        }
+        public ActionResult Smple2()
+        {
+            return View("Smple1");
+        }
+
+        [HttpPost]
+        public ActionResult Smple2(FormCollection form)
+        {
+            return Content(form["UserName"] + "/" + form["Password"] + "/" + form["ConfirmPassword"]);
+        }
+
+        public ActionResult Complex1()
+        {
+            return View("Smple1");
+        }
+
+        [HttpPost]
+        public ActionResult Complex1(SimpleViewModel item)
+        {
+            return Content("Complex1:" + item.UserName + "<br />" + item.Password + "/" + item.ConfirmPassword);
+        }
+
+        public ActionResult Complex2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Complex2(SimpleViewModel item1, SimpleViewModel item2)
+        {
+            return Content("Complex1:" + item1.UserName + "/" + item1.Password + "/" + item1.ConfirmPassword + "<br>"
+                + item2.UserName + "/" + item2.Password + "/" + item2.ConfirmPassword);
+        }
+
+        public ActionResult Complex2ex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Complex2ex(SimpleViewModel item1, SimpleViewModel item2)
+        {
+            return Content("Complex2:" + item1.UserName + "/" + item1.Password + "/" + item1.ConfirmPassword + "<br>"
+                + item2.UserName + "/" + item2.Password + "/" + item2.ConfirmPassword);
+        }
+
+        public ActionResult Complex3()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Complex3(
+            [Bind(Prefix = "item")]
+            SimpleViewModel item1)
+        {
+            return Content("Complex3:" + item1.UserName + "<br />" + item1.Password);
+        }
+
+        public ActionResult Complex4()
+        {
+            var data = from p in db.Client
+                       select new SimpleViewModel
+                       {
+                           UserName = p.FirstName,
+                           Password = p.LastName,
+                           ConfirmPassword = p.MiddleName
+                       };
+            return View(data.Take(10));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Complex4(IList<SimpleViewModel> item)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Complex4:");
+            for (int i = 0; i < item.Count; i++)
+            {
+                sb.Append(item[i].UserName + "/" + item[i].Password + "/" + item[i].ConfirmPassword);
+
+                if (i < item.Count - 1)
+                {
+                    sb.Append("<br/>");
+                }
+            }
+            return Content(sb.ToString());
         }
     }
 }
---
+++ MVC5/Models/Smple1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC5.Models
+{
+    public class SimpleViewModel : IValidatableObject
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+
+        // 可自訂模型驗證
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            yield break;
+        }
+    }
+
+}
