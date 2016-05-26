9549f08c329fe034400a58ae609ae8c228c77f56
jellus156 <jellus156@gmail.com>
2016-05-26 12:03:46 +0800

    2016/05/26

--- MVC5/Controllers/ViewController.cs
+++ MVC5/Controllers/ViewController.cs
@@ -8,8 +8,28 @@ namespace MVC5.Controllers
 {
     public class ViewController : Controller
     {
+        public ActionResult Index()
+        {
+            ViewBag.IntArray = new int[] { 1, 2, 3, 4, 5 };
+            return PartialView();
+        }
         // GET: View
         public ActionResult Example1()
+        {
+            return View();
+        }
+
+        public ActionResult Example3()
+        {
+            return View();
+        }
+
+        public ActionResult Example4()
+        {
+            return View();
+        }
+
+        public ActionResult Example5()
         {
             return View();
         }
