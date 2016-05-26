f843f5ec8924a47255f646a601b228c1383acab8
jellus156 <jellus156@gmail.com>
2016-05-26 14:58:05 +0800

    123

---
+++ MVC5/ActionFilter/LoggerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVC5.ActionFilter
+{
+    public class LoggerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string sActionName = filterContext.ActionDescriptor.ActionName;
+            string sControllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            Trace.TraceInformation(sControllerName + " " + sActionName + " Logger Start!!");
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            string sActionName = filterContext.ActionDescriptor.ActionName;
+            string sControllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            Trace.TraceInformation(sControllerName + " " + sActionName + " Logger End!!");
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ MVC5/ActionFilter/SharedDataAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVC5.ActionFilter
+{
+    public class SharedDataAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // 將原本要顯示的訊息，或執行內容，在此Action執行前先在此執行完畢後再呼叫Action顯示View
+            filterContext.Controller.ViewBag.Message = "!!!!! Your application description page.";
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
+
